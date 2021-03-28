e738422f8e929606c957444a74ca9a007bb1f467
mack akdas <mack@b-structures.com>
2021-03-28 21:26:34 +0200

    make it better

--- lib/router.ex
+++ lib/router.ex
@@ -5,12 +5,12 @@ defmodule SimpleAppRouter do
   plug :dispatch
 
   get "/bpi" do
-    #send_resp(conn, 200, Coinbase.bpi_current_price)
-    send_resp(conn, 404, "this is bpi")
+    send_resp(conn, 200, Coinbase.bpi_current_price)
+    #send_resp(conn, 404, "this is bpi")
 
   end
 
   match _ do
-    send_resp(conn, 404, "Wrong place mate")
+    send_resp(conn, 404, "Wrong place mate, move along")
   end
 end
